9dd0abc53bd477d3c877db607963f4b1966023f9
Paulin85 <46808735+Paulin85@users.noreply.github.com>
2021-11-10 16:51:16 +0100

    feat : add css response

--- src/components/Response.vue
+++ src/components/Response.vue
@@ -5,7 +5,9 @@
         <p id="result"> OK !</p>
     </div>
     <div class="data">
-
+        <!-- <v-for></v-for> -->
+        <p id="state"> Etat 200</p>
+        <p id="result"> OK !</p>
     </div>
   </div>
 </template>
@@ -15,5 +17,28 @@ export default { name: "Response", request: "" };
 </script>
 
 <style scoped>
+
+.response {
+    margin: 2vw 25vw;
+    color: black;
+    font-weight: bold;
+}
+
+.state {
+    background-color:rgba(165, 165, 165, 0.281);
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    border-radius: 10px;
+}
+
+
+.data {
+    background-color:rgba(165, 165, 165, 0.281);
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+    border-radius: 10px;
+}
 
 </style>
