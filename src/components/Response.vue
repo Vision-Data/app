e3e94637c247abfa90845e88dfdafa91d5b22b92
Paulin85 <46808735+Paulin85@users.noreply.github.com>
2021-11-10 15:42:01 +0100

    feat : create component response

---
+++ src/components/Response.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="response">
+    <div class="state">
+        <p id="state"> Etat 200</p>
+        <p id="result"> OK !</p>
+    </div>
+    <div class="data">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default { name: "Response", request: "" };
+</script>
+
+<style scoped>
+
+</style>
