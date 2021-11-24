560e8a2c84724a6a5a7ce8eef01aab1fac272c3b
Paulin85 <46808735+Paulin85@users.noreply.github.com>
2021-11-24 11:42:10 +0100

    feat: add array & add data & styling

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,39 +1,126 @@
 <template>
+  <section>
+    <div class="alert alert-object alert-info">
+      <div class="flex-1">
+        <label class="tag-title">
+          <button class="badge bg-info border-transparent">Users</button>
+        </label>
+        <div class="alert alert-info" v-for="user in users" :key="user.data">
+          <div class="flex-1">
+            <label>
+              <button class="badge bg-info border-transparent">Nom</button>
+              {{user.nom === '' ? "(vide)" : user.nom }} 
+            </label>
+            <label>
+              <button class="badge bg-info border-transparent">Prénom</button>
+              {{ user.prenom }}
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    <div class="alert alert-info" v-for="object in objects" :key="object.data">
-    <div class="flex-1">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>                          
-        </svg> 
-        <label> {{ object.prenom }} {{ object.nom }} </label>
+    <div class="alert alert-object alert-warning">
+      <div class="flex-1">
+        <label>
+          <button class="tag-title badge bg-warning border-transparent">Person</button>
+        </label>
+            <label>
+              <button class="badge bg-warning border-transparent">Hair</button>
+              {{ person.hair }}
+            </label>
+            <label>
+              <button class="badge bg-warning border-transparent">
+                Taille
+              </button>
+              {{ person.taille }}
+            </label>
+      </div>
     </div>
-    </div>
-    
+  </section>
 </template>
 
 
 <script>
-    export default {
-        name: "Data",
-        components: {
+export default {
+  name: "Data",
+  components: {},
+  data() {
+    return {
+      users: [
+        {
+          prenom: "Martin",
+          nom: "Dupont",
         },
-        data() {
-            return {
-                objects : [{
-                    prenom : 'Arthur',
-                    nom : 'Geay'
-                }, 
-                {
-                    prenom : 'Jeremie',
-                    nom : 'Delecrin'
-                }]
-            }
-        }
+        {
+          prenom: "Martin",
+          nom: "Dupont",
+        },
+        {
+          prenom: "Martin",
+          nom: "Dupont",
+        },
+        {
+          prenom: "Martin",
+          nom: "Dupont",
+        },
+        {
+          prenom: "Martin",
+          nom: "Dupont",
+        },
+        {
+          prenom: "Martin",
+          nom: "Dupont",
+        },
+        {
+          prenom: "Martin",
+          nom: "",
+        },
+      ],
+      id: 1,
+      person: {
+        hair: "Marron",
+        taille: "178cm",
+      },
     };
+  },
+};
 </script>
 
  <style >
-     .alert {
-         margin: 10px;
-     }
- </style>
+.alert {
+  margin: 10px;
+
+}
+
+.alert label {
+  margin-right: 10px;
+  max-width: 10rem;
+  overflow-wrap: break-word;
+}
+
+.alert .tag-title {
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+
+
+.alert .flex-1 {
+    display: flex;
+    flex-wrap: wrap;
+    position: relative;
+    align-items: flex-start;
+
+}
+
+.alert-object > .flex-1 {
+    padding: 10px ;
+    padding-left: 70px;
+}
+
+.badge {
+    margin-right: 5px;
+}
+
+</style>
--- src/components/Response.vue
+++ src/components/Response.vue
@@ -1,26 +1,38 @@
 <template>
   <div class="response">
     <div class="alert alert-success">
-    <div class="flex-1">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">          
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>                
-        </svg> 
-        <label>Etat 200 : OK !</label>
+      <div class="flex-1">
+        <label>
+          <button class="badge bg-success border-transparent">Etat</button> 200
+          : OK !</label
+        >
+      </div>
     </div>
+
+    <div class="alert alert-info">
+      <div class="flex-1">
+        <label>
+          <button class="badge bg-info border-transparent">ID</button> {{ id }}
+        </label>
+      </div>
     </div>
 
     <Data />
-    
   </div>
 </template>
 
 <script>
-    import Data from "./Data.vue";
-    export default {
-        name: "Response",
-        components: {
-            Data
-        },
+import Data from "./Data.vue";
+export default {
+  name: "Response",
+  components: {
+    Data,
+  },
+  data() {
+    return {
+      id: 1,
     };
+  },
+};
 </script>
 
