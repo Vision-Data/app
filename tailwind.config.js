e3e2a51e2779f803818f5bfb855c080c64d54c1d
Arthur Geay <arthurgeay.contact@gmail.com>
2021-11-24 10:29:39 +0100

    feat: add custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,7 +7,33 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        visionTheme: {
+          primary: "#fe932f",
+          "primary-focus": "#fa810f",
+          "primary-content": "#ffffff",
+          secondary: "#fab700",
+          "secondary-focus": "#eba800",
+          "secondary-content": "#ffffff",
+          accent: "#fab700",
+          "accent-focus": "#eba800",
+          "accent-content": "#ffffff",
+          neutral: "#3d4451",
+          "neutral-focus": "#2a2e37",
+          "neutral-content": "#ffffff",
+          "base-100": "#ffffff",
+          "base-200": "#f9fafb",
+          "base-300": "#d1d5db",
+          "base-content": "#1f2937",
+          info: "#2094f3",
+          success: "#009485",
+          warning: "#ff9900",
+          error: "#ff5724",
+        },
+      },
+    ],
+  },
+};
