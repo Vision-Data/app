b70df57768386878ebe20ea5ca630d7fea3b959d
Yessi Munguengui <yessimunguengui@gmail.com>
2021-12-08 09:27:31 +0100

    fix : make searchbar responsive

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
-  <div class="m-4">
-    <header class="flex justify-center items-center">
-      <img
-        class="p-10"
-        alt="Vue logo"
-        src="./assets/color-logo.svg"
-        width="150"
+  <div class="flex justify-center mt-10 ">
+    <header>
+      <SearchBar
+        class="container w-full md:w-screen max-w-screen-lg md:-mx-60"
       />
-      <SearchBar class="w-8/12" />
     </header>
   </div>
 </template>
