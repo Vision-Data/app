0fa919482b6310550623b1504c0f21f4fa2acdb4
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-04-07 16:32:09 +0200

    fix: style on register and login

--- src/views/Authentication/Login.vue
+++ src/views/Authentication/Login.vue
@@ -1,77 +1,78 @@
 <template>
-  <img
-    id="logo"
-    class="w-96 m-auto"
-    :src="require(`@/assets/watermark-color.png`)"
-    alt="logo-vision"
-  />
-  <h1 class="text-center font-bold text-4xl mt-4">Connexion</h1>
-  <section class="flex flex-col justify-center items-center mt-8">
-    <div class="w-96 mb-2">
-      <div class="flex flex-col">
-        <Button class="btn-lg">
-          <img :src="require(`@/assets/LogoGoogle.png`)" class="w-8 mr-4" />
-          Connexion avec Google
-        </Button>
-        <Button class="btn-lg mt-4">
-          <img :src="require(`@/assets/LogoGithub.png`)" class="w-8 mr-4" />
-          Connexion avec Github
-        </Button>
-      </div>
-    </div>
-    <div class="divider divider-horizontal font-bold">
-      OU
-    </div>
-    <div class="flex flex-col items-center w-96">
-      <Alert
-        type="error"
-        :label="errors.message"
-        v-if="errors && errors.message"
-      />
-      <form class="w-full max-w-xs" @submit.prevent="login">
-        <div class="form-control w-full max-w-xs">
-          <label class="label">
-            <span class="label-text">Adresse e-mail</span>
-          </label>
-          <input
-            type="text"
-            placeholder="Adresse e-mail"
-            class="input input-bordered w-full max-w-xs"
-            :class="{ 'input-error': errors && errors.email }"
-            v-model="email"
-          />
-          <ErrorLabel :label="errors.email" v-if="errors && errors.email" />
+  <div class="login">
+    <img
+      id="logo"
+      class="w-96"
+      :src="require(`@/assets/watermark-color.png`)"
+      alt="logo-vision mb-8"
+    />
+    <h1 class="text-center font-bold text-4xl mt-4">Connexion</h1>
+    <section class="flex flex-col justify-center items-center mt-8">
+      <!-- <div class="w-96 mb-2">
+        <div class="flex flex-col">
+          <Button class="btn-lg">
+            <img :src="require(`@/assets/LogoGoogle.png`)" class="w-8 mr-4" />
+            Connexion avec Google
+          </Button>
+          <Button class="btn-lg mt-4">
+            <img :src="require(`@/assets/LogoGithub.png`)" class="w-8 mr-4" />
+            Connexion avec Github
+          </Button>
         </div>
-        <div class="form-control w-full max-w-xs mt-2">
-          <label class="label">
-            <span class="label-text">Mot de passe</span>
-          </label>
-          <input
-            type="password"
-            placeholder="Mot de passe"
-            class="input input-bordered w-full max-w-xs"
-            :class="{ 'input-error': errors && errors.password }"
-            v-model="password"
-          />
-          <ErrorLabel
-            :label="errors.password"
-            v-if="errors && errors.password"
-          />
-        </div>
-        <div class="form-control w-full max-w-xs mt-8">
-          <Button class="btn-primary" @click="login" :isLoading="isLoading"
-            >Connexion</Button
+      </div> -->
+      <!-- <div class="divider divider-horizontal font-bold">
+        OU
+      </div>  -->
+      <div class="flex flex-col items-center w-96">
+        <Alert
+          type="error"
+          :label="errors.message"
+          v-if="errors && errors.message"
+        />
+        <form class="w-full max-w-xs" @submit.prevent="login">
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Adresse e-mail</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Adresse e-mail"
+              class="input input-bordered w-full max-w-xs"
+              :class="{ 'input-error': errors && errors.email }"
+              v-model="email"
+            />
+            <ErrorLabel :label="errors.email" v-if="errors && errors.email" />
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Mot de passe</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Mot de passe"
+              class="input input-bordered w-full max-w-xs"
+              :class="{ 'input-error': errors && errors.password }"
+              v-model="password"
+            />
+            <ErrorLabel
+              :label="errors.password"
+              v-if="errors && errors.password"
+            />
+          </div>
+          <div class="form-control w-full max-w-xs mt-4">
+            <Button class="btn-primary" @click="login" :isLoading="isLoading"
+              >Connexion</Button
+            >
+          </div>
+        </form>
+        <p class="text-center mt-4">
+          <router-link to="/register" class="link-primary"
+            >Vous n'avez pas de compte ? Inscrivez-vous !</router-link
           >
-        </div>
-      </form>
-
-      <p class="text-center mt-4">
-        <router-link to="/register" class="link-primary"
-          >Vous n'avez pas de compte ? Inscrivez-vous !</router-link
-        >
-      </p>
-    </div>
-  </section>
+        </p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -112,3 +113,13 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.login {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- src/views/Authentication/Registration.vue
+++ src/views/Authentication/Registration.vue
@@ -1,96 +1,98 @@
 <template>
-  <img
-    id="logo"
-    class="w-96 m-auto"
-    :src="require(`@/assets/watermark-color.png`)"
-    alt="logo-vision"
-  />
-  <h1 class="text-center font-bold text-4xl mt-4">Inscription</h1>
-  <section
-    class="flex flex-col md:flex-row justify-center md:space-x-10 items-center mt-8"
-  >
-    <Alert
-      type="error"
-      :label="errors.message"
-      v-if="errors && errors.message"
-    />e
-    <form class="flex flex-col items-center w-96" @submit.prevent="register">
-      <div class="form-control w-full max-w-xs">
-        <label class="label">
-          <span class="label-text">Adresse e-mail</span>
-        </label>
-        <input
-          type="text"
-          placeholder="Adresse e-mail"
-          class="input input-bordered w-full max-w-xs"
-          :class="{ 'input-error': errors && errors.email }"
-          v-model="email"
-        />
-        <ErrorLabel :label="errors.email" v-if="errors && errors.email" />
-      </div>
-      <div class="form-control w-full max-w-xs mt-2">
-        <label class="label">
-          <span class="label-text">Pseudo</span>
-        </label>
-        <input
-          type="text"
-          placeholder="Pseudo"
-          class="input input-bordered w-full max-w-xs"
-          :class="{ 'input-error': errors && errors.full_name }"
-          v-model="pseudo"
-        />
-        <ErrorLabel
-          :label="errors.full_name"
-          v-if="errors && errors.full_name"
-        />
-      </div>
-      <div class="form-control w-full max-w-xs mt-2">
-        <label class="label">
-          <span class="label-text">Mot de passe</span>
-        </label>
-        <input
-          type="password"
-          placeholder="Mot de passe"
-          class="input input-bordered w-full max-w-xs"
-          :class="{ 'input-error': errors && errors.password }"
-          v-model="password"
-        />
-        <input
-          type="password"
-          placeholder="Confirmation du mot de passe"
-          class="input input-bordered w-full max-w-xs"
-          :class="{ 'input-error': errors && errors.password }"
-          v-model="passwordConf"
-        />
-        <ErrorLabel :label="errors.password" v-if="errors && errors.password" />
-      </div>
-      <div class="form-control w-full max-w-xs mt-8">
-        <Button class="btn-primary" @click="register" :isLoading="isLoading"
-          >Inscription</Button
-        >
-      </div>
-    </form>
-    <div class="divider divider-horizontal font-bold">
-      OU
-    </div>
-    <div class="w-96">
-      <div class="flex flex-col">
-        <Button class="btn-lg">
-          <img :src="require(`@/assets/LogoGoogle.png`)" class="w-8 mr-4" />
-          Connexion avec Google
-        </Button>
-        <Button class="btn-lg mt-4 mb-8">
-          <img :src="require(`@/assets/LogoGithub.png`)" class="w-8 mr-4" />
-          Connexion avec Github
-        </Button>
-        <p class="text-center">
-          <router-link to="/login" class="link-primary"
-            >Vous avez déjà un compte ? Connectez-vous !</router-link
+  <div class="register">
+    <img
+      id="logo"
+      class="w-96 mb-8"
+      :src="require(`@/assets/watermark-color.png`)"
+      alt="logo-vision"
+    />
+    <h1 class="text-center font-bold text-4xl mt-4">Inscription</h1>
+    <section
+      class="flex flex-col md:flex-row justify-center md:space-x-10 items-center mt-8"
+    >
+      <Alert
+        type="error"
+        :label="errors.message"
+        v-if="errors && errors.message"
+      />
+      <form class="flex flex-col items-center w-96" @submit.prevent="register">
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text">Adresse e-mail</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Adresse e-mail"
+            class="input input-bordered w-full max-w-xs"
+            :class="{ 'input-error': errors && errors.email }"
+            v-model="email"
+          />
+          <ErrorLabel :label="errors.email" v-if="errors && errors.email" />
+        </div>
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text">Pseudo</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Pseudo"
+            class="input input-bordered w-full max-w-xs"
+            :class="{ 'input-error': errors && errors.full_name }"
+            v-model="pseudo"
+          />
+          <ErrorLabel
+            :label="errors.full_name"
+            v-if="errors && errors.full_name"
+          />
+        </div>
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text">Mot de passe</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            class="input input-bordered w-full max-w-xs"
+            :class="{ 'input-error': errors && errors.password }"
+            v-model="password"
+          />
+          <input
+            type="password"
+            placeholder="Confirmation du mot de passe"
+            class="input input-bordered w-full max-w-xs"
+            :class="{ 'input-error': errors && errors.password }"
+            v-model="passwordConf"
+          />
+          <ErrorLabel :label="errors.password" v-if="errors && errors.password" />
+        </div>
+        <div class="form-control w-full max-w-xs mt-4">
+          <Button class="btn-primary" @click="register" :isLoading="isLoading"
+            >Inscription</Button
           >
-        </p>
-      </div>
-    </div>
-  </section>
+        </div>
+      <p class="text-center mt-4">
+        <router-link to="/login" class="link-primary"
+          >Vous avez déjà un compte ? Connectez-vous !</router-link
+        >
+      </p>
+      </form>
+      <!-- <div class="divider divider-horizontal font-bold">
+        OU
+      </div> -->
+      <!-- <div class="w-96">
+        <div class="flex flex-col">
+          <Button class="btn-lg">
+            <img :src="require(`@/assets/LogoGoogle.png`)" class="w-8 mr-4" />
+            Connexion avec Google
+          </Button>
+          <Button class="btn-lg mt-4 mb-8">
+            <img :src="require(`@/assets/LogoGithub.png`)" class="w-8 mr-4" />
+            Connexion avec Github
+          </Button>
+        </div>
+      </div> -->
+    </section>
+  </div>
 </template>
 
 <script>
@@ -141,3 +143,13 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.register {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
